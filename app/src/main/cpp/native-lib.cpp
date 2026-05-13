f7125b1e3b4bed6df6438328b59167274d4964ed
olekslukian <alexlukianoff@gmail.com>
2026-05-13 17:18:13 +0200

    Initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,11 @@
+#include <jni.h>
+#include <string>
+#include <opencv2/opencv.hpp>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_xyz_olekslukian_simplenotes_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}/
